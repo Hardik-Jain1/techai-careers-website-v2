583588d4b32ea73ff69e3d6930d7a6c2786d2c68
Hardik-Jain1 <100846110+Hardik-Jain1@users.noreply.github.com>
2023-06-29 21:39:54 +0000

    store application in db

--- database.py
+++ database.py
@@ -21,9 +21,28 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),{'val': id})
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                          {'val': id})
     rows = result.all()
     if len(rows) == 0:
       return None
     else:
       return rows[0]._asdict()
+
+
+def add_application_to_db(id, data):
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO applications(job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+
+    conn.execute(
+      query, {
+        'job_id': id,
+        'full_name': data["full_name"],
+        'email': data["email"],
+        'linkedin_url': data["linkedin_url"],
+        'education': data["education"],
+        'work_experience': data["work_experience"],
+        'resume_url': data["resume_url"]
+      })
